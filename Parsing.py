2a42722679fb11f9b0f3d21a5e31f601303eb95e
ekb96 <ekb96.13@mail.ru>
2019-10-25 22:21:18 +0500

    Add Excel

--- Parsing.py
+++ Parsing.py
@@ -1,73 +1,86 @@
 # Данный скрипт осуществляет парсинг Avito.
 # Получает заголовок объявления, цену, дату/время, ссылку.
+# Запись результата в файл Excel/SQL;
 
 # Необходимо добавить:
 # проверку с заданными промежутками времени;
-# запись результата в файл Excel/SQL;
-# добавление в результаты при появлении новых объявлений;
+# добавление при появлении новых объявлений;
+# сортировку результатов по времени добавления;
 # оповещение на Email о новых объявлениях.
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup															# модуль для работы с html
 import re																				# регулярные выражения
 import openpyxl																			# библиотека для Excel
+import datetime																			# модуль для работы с датой
 
 def get_html(url):
 	r = requests.get(url)
 	return r.text
 	
 def get_total_pages(html):
+	now = datetime.datetime.now()														# получаем дату
+	#print ("Текущий год: %d" % now.year)												# отделяем год
+	#print ("Текущий месяц: %d"% now.month)												# отделяем месяц
+	#print ("Текущий день: %d" % now.day)												# отделяем день
+	
 	index_pages = 0																		# начальный индекс
 	soup = BeautifulSoup(html, 'lxml')
 																						# получаем список
 	pages = soup.find_all ('div', attrs = {'class': 'description item_table-description'})
 	
 	pages_len = len(pages)																# кол-во элементов в списке
-	print(str(pages_len) + '\n')
+	#print(str(pages_len) + '\n')
 	
 	try:
-		# читаем excel-файл
-		wb = openpyxl.load_workbook('base.xlsx')
-	except IOError:
-		# создаем новый excel-файл
-		wb = openpyxl.Workbook()
+		wb = openpyxl.load_workbook('base.xlsx')										# читаем excel-файл
+	except IOError:																		# если файла нет
+		wb = openpyxl.Workbook()														# создаем новый excel-файл
 		wb.create_sheet(title = 'Объявления', index = 0)
 	
-	# получаем лист, с которым будем работать
-	sheet = wb['Объявления']
+	sheet = wb['Объявления']															# получаем лист, с которым будем работать
 	
 	while (index_pages != pages_len):													# цикл обхода списка
 		pages1 = pages[index_pages]														# получаем элемент списка
 
-		title =  pages1.find ('span', attrs = {'itemprop': 'name'})						# достаем тег "name"
-		price =  pages1.find ('span', attrs = {'class': 'price'})						# достаем тег "price"
-		date =  pages1.find ('div', attrs = {'class': 'item-date'})						# достаем тег "item-date"
+		title = pages1.find ('span', attrs = {'itemprop': 'name'})						# достаем тег "name"
+		price = pages1.find ('span', attrs = {'class': 'price'})						# достаем тег "price"
+		date = pages1.find ('div', class_ = 'js-item-date c-2').get('data-absolute-date')
 		href = pages1.find('a', class_ = 'item-description-title-link').get('href')		# достаем ссылку
 	
 		title = str(title.get_text())													# достаем заголовок
 		price = str(price.get_text())													# достаем цену
-		date = str(date.get_text())														# достаем дату
-		href = "http:/" + str(href)
+		date = str(date)																# достаем дату и время
+		href = "http://avito.ru" + str(href)											# дописываем ссылку
 		
 		title = re.sub("^\s+|\n|\r|\s+$", '', title)									# удаляем лишние символы
 		price = re.sub("^\s+|\n|\r|₽|\s+$", '', price)									# удаляем лишние символы
 		date = re.sub("^\s+|\n|\r|\s+$", '', date)										# удаляем лишние символы
-
+		
+		if date.find("Сегодня") == 0:
+			date1 = date.split(' ')														# разделяем строку по ' '
+			date1[0] = "%d" % now.day + "." + "%d" % now.month + "." + "%d" % now.year
+			date = date1[0] + " " + date1[1]
+			print(date)
+		elif date.find("Вчера") == 0:
+			date1 = date.split(' ')														# разделяем строку по ' '
+			date1[0] = str(int("%d" % now.day)-1) + "." + "%d" % now.month + "." + "%d" % now.year
+			date = date1[0] + " " + date1[1]
+			print(date)
+		else:
+			print(date)
+		
 		print(title)
 		print(price)
-		print(date)
 		print(href + '\n')
 		
-		# запись в файл
-		sheet.append([title, price, date, href])
-		
+		sheet.append([date, title, price, href])										# запись в файл
 		index_pages += 1																# индекс следующего элемента
 	
-	# сохраняем файл
-	wb.save('base.xlsx')	
+	wb.save('base.xlsx')																# сохраняем файл
 	
 def main():
-	url = "https://www.avito.ru/ekaterinburg/avtomobili/s_probegom/inomarki/mehanika/benzin/levyy_rul/ne_bolee_dvuh/ne_bityy?cd=1&pmax=350000&pmin=150000&radius=200&user=1&f=188_901b20303"
+	url = "https://www.avito.ru/ekaterinburg/avtomobili/s_probegom/inomarki/mehanika/benzin/levyy_rul/ne_bolee_dvuh/ne_bityy?cd=1&pmax=350000&pmin=150000&radius=200&s=104&user=1&f=188_901b20303"
 	
 	html = get_html(url)
 	get_total_pages(html)
